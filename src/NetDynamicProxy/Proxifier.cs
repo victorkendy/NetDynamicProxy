609f64ade35f8862be146635bf2600fd08a739c6
Victor Harada <victorkendy@gmail.com>
2017-03-01 20:41:39 -0300

    Proxifier public interface with empty implementation

---
+++ src/NetDynamicProxy/Proxifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetDynamicProxy
+{
+    public class Proxifier
+    {
+        public static ProxifierWithBaseClass<T> For<T>() where T:class
+        {
+            return new ProxifierWithBaseClass<T>();
+        }
+    }
+
+    public class ProxifierWithBaseClass<T> where T:class
+    {
+        private IList<Type> implementedInterfaces = new List<Type>();
+
+        public ProxifierWithBaseClass<T> WithInterfaces(params Type[] interfaces)
+        {
+            foreach(Type i in interfaces)
+            {
+                implementedInterfaces.Add(i);
+            }
+            return this;
+        }
+
+        public T Build()
+        {
+            return null;
+        }
+    }
+}
---
+++ test/NetDynamicProxy.Tests/ProxifierTest.cs
@@ -0,0 +1,15 @@
+﻿using NetDynamicProxy;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class ProxifierTest
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.IsType(typeof(object), Proxifier.For<Object>().Build());
+        }
+    }
+}
